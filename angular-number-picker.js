6deb3c3eca09ddf79b0e29e8566a7d51db497594
Elad Karakuli <elad.karakuli@gmail.com>
2015-11-27 15:30:02 +0200

    checking if reuire is defined

--- angular-number-picker.js
+++ angular-number-picker.js
@@ -175,7 +175,7 @@
         return name;
     };
 
-    if (typeof exports === 'object') {
+    if (typeof exports === 'object' && require) {
         module.exports = definition(require('angular'));
     } else if (typeof define === 'function' && define.amd) {
         define(['angular'], definition);
